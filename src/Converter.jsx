75587f814156c9bad0a8dabb29ec05edf94bab3c
ravirigz <gapuzralphvincent@gmail.com>
2025-11-27 21:07:33 +0800

    Added design elements

---
+++ src/Converter.jsx
@@ -0,0 +1,69 @@
+import { useState } from 'react'
+import './Converter.css'
+import './Converter.js'
+
+function Converter() {
+    return (
+        <>
+            <div class="main-container">
+                <div class="column-container">
+                    <div class="converter">
+                        <h2>💵 Currency Converter</h2>
+                        <label class="label">Amount (USD):</label><br />
+                        <input class="input" type="number" id="inputAmount" placeholder="Amount" /><br /><br />
+                        <label class="label">Convert to:</label><br />
+                        <select class="dropdown" id="convertTo">
+                            <option value="EUR">Euro</option>
+                            <option value="PHP">Philippine Peso</option>
+                        </select><br /><br />
+                        <button class="button" onClick={convertCurrency} id="buttonCurrency">Convert</button>
+                        <p class="textDOM" id="textCurrency"></p>
+                    </div>
+
+                    <div class="converter">
+                        <h2>🛣️ Distance Converter</h2>
+                        <label class="label">Distance (Miles):</label><br />
+                        <input class="input" type="number" id="inputUnit" placeholder="Distance" /><br /><br />
+                        <label class="label">Convert to:</label><br />
+                        <select class="dropdown" id="convertUnit">
+                            <option value="M">Meters</option>
+                            <option value="KM">Kilometers</option>
+                        </select><br /><br />
+                        <button class="button" onClick={convertUnit} id="buttonUnit">Convert</button>
+                        <p class="textDOM" id="textUnit"></p>
+                    </div>
+                </div>
+
+                <div class="column-container">
+                    <div class="converter">
+                        <h2>🌡️ Temperature Converter</h2>
+                        <label class="label">Amount (USD):</label><br />
+                        <input class="input" type="number" id="inputAmount" placeholder="Amount" /><br /><br />
+                        <label class="label">Convert to:</label><br />
+                        <select class="dropdown" id="convertTo">
+                            <option value="EUR">Euro</option>
+                            <option value="PHP">Philippine Peso</option>
+                        </select><br /><br />
+                        <button class="button" onClick={convertCurrency} id="buttonTemperature">Convert</button>
+                        <p class="textDOM" id="textCurrency"></p>
+                    </div>
+
+                    <div class="converter">
+                        <h2>⏰ Time Converter</h2>
+                        <label class="label">Distance (Miles):</label><br />
+                        <input class="input" type="number" id="inputUnit" placeholder="Distance" /><br /><br />
+                        <label class="label">Convert to:</label><br />
+                        <select class="dropdown" id="convertUnit">
+                            <option value="M">Meters</option>
+                            <option value="KM">Kilometers</option>
+                        </select><br /><br />
+                        <button class="button" onClick={convertUnit} id="buttonTime">Convert</button>
+                        <p class="textDOM" id="textUnit"></p>
+                    </div>
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default Converter
--- src/main.jsx
+++ src/main.jsx
@@ -1,17 +1,11 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './Converter.css'
-import Converter1 from './Converter1.jsx'
-import Converter2 from './Converter2.jsx'
+import Converter1 from './Converter.jsx'
+// import Converter2 from './Converter2.jsx'
 
-createRoot(document.getElementById('converter1')).render(
+createRoot(document.getElementById('converter')).render(
   <StrictMode>
     <Converter1 />
-  </StrictMode>,
-)
-
-createRoot(document.getElementById('converter2')).render(
-  <StrictMode>
-    <Converter2 />
   </StrictMode>,
 )
